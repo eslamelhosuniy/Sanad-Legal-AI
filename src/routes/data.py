7ce2554e45cad712109e232df8c3547c71bdbb47
Your Name <youremail@example.com>
2025-09-14 13:46:40 +0300

    enhancment responses with Enums & JSONResponse

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI,APIRouter,Depends,UploadFile
+from fastapi import FastAPI,APIRouter,Depends,UploadFile,status
+from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
 from controllers import DataController
+from models import ResponseSignal
+
 import os
 
 data_router = APIRouter(prefix="/api/v1/data",tags = ["api v1/data"])
@@ -9,6 +12,25 @@ data_router = APIRouter(prefix="/api/v1/data",tags = ["api v1/data"])
 async def upload_data(project_id:str , file:UploadFile,app_settings: Settings = Depends(get_settings)):
 
         is_valid,msg = DataController().validate_uploaded_file(file=file)
-        return {
-                is_valid:is_valid,
-                msg : msg}
+
+        if not is_valid:
+                return JSONResponse (
+                
+                        status_code = status.HTTP_400_BAD_REQUEST,
+                        content = {
+                                "status":status.HTTP_400_BAD_REQUEST,
+                                "msg" : msg
+                        
+                }
+                )
+        else:
+                 return JSONResponse (
+                         
+                        status_code = status.HTTP_200_OK,
+                        content = {
+                                "status":status.HTTP_200_OK,
+                                "msg" : msg
+                        }
+                 )
+                
+
