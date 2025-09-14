060e1b7bccdf2dc912e2e50024a6f06b34e373fd
Your Name <youremail@example.com>
2025-09-14 18:05:35 +0300

    add logger

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,4 +1,4 @@
-from asyncio.log import logger
+import logging
 from fastapi import FastAPI,APIRouter,Depends,UploadFile,status
 from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
@@ -8,6 +8,7 @@ from models import ResponseSignal
 import os
 
 data_router = APIRouter(prefix="/api/v1/data",tags = ["api v1/data"])
+logger = logging.getLogger('uvicorn.error')
 
 @data_router.post("/upload/{project_id}")
 async def upload_data(project_id:str , file:UploadFile,app_settings: Settings = Depends(get_settings)):
@@ -30,7 +31,17 @@ async def upload_data(project_id:str , file:UploadFile,app_settings: Settings = 
                           while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
                                   await f.write(chunk)
                  except Exception as e:
-                         logger.error(f"Error while uploading file: {e}")
+                          logger.error(f"Error while uploading file:{e}")
+                          return JSONResponse (
+                
+                        status_code = status.HTTP_400_BAD_REQUEST,
+                        content = {
+                                "status":status.HTTP_400_BAD_REQUEST,
+                                "msg" : ResponseSignal.FILE_UPLOAD_FAILED
+                        
+                }
+                )
+
                          
                  project_dir_path = ProjectController().get_project_path(project_id=project_id)
                  return JSONResponse (
