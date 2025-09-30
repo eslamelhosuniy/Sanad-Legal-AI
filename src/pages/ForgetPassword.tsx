a2ba63abd5bafdce2d328e9865c000e34fe1c37a
adham3madd <abdelmawlaadhm58@gmail.com>
2025-09-30 13:20:45 +0300

    Update ForgetPassword.tsx

--- src/pages/ForgetPassword.tsx
+++ src/pages/ForgetPassword.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Mail, Loader2, Scale, Globe } from "lucide-react";
-import { Link, useNavigate } from "react-router-dom"; // ✅ أضف useNavigate هنا
+import { Link, useNavigate } from "react-router-dom";
 import { t } from "i18next";
 import ChangeLanguageButton from "../components/UI/ChangeLanguageButton";
 
@@ -9,7 +9,7 @@ export default function ForgetPassword() {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
     const [success, setSuccess] = useState<string | null>(null);
-    const navigate = useNavigate(); // ✅ هنا
+    const navigate = useNavigate(); 
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
